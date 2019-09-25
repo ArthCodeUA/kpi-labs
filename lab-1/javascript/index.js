07db35b09a79dfab94c76139f12e294e6bdfe109
Arthur <arthionweb@gmail.com>
2019-09-25 11:46:25 +0300

    Updated JS Part

--- lab-1/javascript/index.js
+++ lab-1/javascript/index.js
@@ -3,11 +3,10 @@
 "use strict";
 
 // Default way of introducing a function
-const APROG = function (a, d, n) {
-    return a && d && n ? a + d * (n - 1) : "Not enough data";
-};
+function aprog(a, d, n) { return a && d && n ? a + d * (n - 1) : "Not enough data"; }
+
+// Semi-default way of introducing a function
+const APROGS = function (a, d, n) { return a && d && n ? a + d * (n - 1) : "Not enough data"; };
 
 // Introducing function using lambda-expression
-const APROGL = (a, d, n) => {
-    return a && d && n ? a + d * (n - 1) : "Not enough data";
-};
+const APROGL = (a, d, n) => { return a && d && n ? a + d * (n - 1) : "Not enough data"; };
