a0310ea7f50b86fcf5ead6b2c50ef3813fcfe294
Arthur <arthionweb@gmail.com>
2019-11-13 11:12:15 +0200

    Lab #6 done and approved.

---
+++ lab-6/index.js
@@ -0,0 +1,10 @@
+// Function h(a,b) is a function, that is being used in function F.
+let h = (a, b) => {
+    return (a / (1 + b ** 2) + (b / (1 + a ** 2)) - (a - b) ** 3);
+};
+
+// Function F, that is solution of the task. Instead of h(1,1) there is just 1 in the end of function.
+// This is because of h(1,1) is a constant value and it equals to 1.
+let f = (s, t) => {
+    return h(s, t) + Math.max(h(s - t, s * t) ** 2, h(s - t, s + t) ** 4) + 1;
+};
